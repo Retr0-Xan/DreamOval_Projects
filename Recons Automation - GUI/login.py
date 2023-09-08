4df9ff3836f6c4731d87719f9ed5e9b2ddee7bd4
Retr0-Xan <mayitey69@gmail.com>
2023-09-08 04:16:31 +0000

    used canvas for transparency effect.(needs work)

--- Recons Automation - GUI/login.py
+++ Recons Automation - GUI/login.py
@@ -19,13 +19,25 @@ def main():
 
     bg_label = ctk.CTkLabel(root, image=bg_img)
     bg_label.pack(fill="both",expand=True)
-    
+
     bg_label.bg_img = bg_img
 
-    logo_img = ImageTk.PhotoImage(Image.open("C:\\Users\\Mark\\repos\DreamOval_Projects\Recons Automation - GUI\\assets\\KowriLogo.png").resize((200,70)))
-    logo_label = ctk.CTkLabel(bg_label, image=logo_img,text="",fg_color="white")
-    logo_label.place(relx=0.05,rely=0.06)
+    # Load the PNG image using PIL
+    image = Image.open("C:\\Users\\Mark\\repos\DreamOval_Projects\Recons Automation - GUI\\assets\\KowriLogo.png")
 
+    # Convert the PIL Image to a PhotoImage
+    img = ImageTk.PhotoImage(image.resize((200,70)))
+
+    # Create a Canvas widget
+    canvas = tk.Canvas(bg_label, width=200, height=70)
+    canvas.place(relx=0.05,rely=0.06)
+
+    # Create a Label on the Canvas to display the transparent image
+    label = tk.Label(canvas, image=img)
+    label.photo = img  # Keep a reference to the image to prevent it from being garbage collected
+
+    # Place the Label on the Canvas
+    canvas.create_window(0, 0, anchor=tk.NW, window=label)
 
     welcome_frame = ctk.CTkFrame(bg_label,fg_color="#01911e",corner_radius=10,bg_color="#ebf0ec",width=400,height=300)
     welcome_frame.pack_propagate(False)
@@ -34,10 +46,6 @@ def main():
     welcome_msg = ctk.CTkLabel(welcome_frame,text="Welcome Back!",text_color="White",font=ctk.CTkFont("Rounded Arial",size = 40,weight="bold"))
     welcome_msg.pack_propagate(False)
     welcome_msg.pack()
-
-    signin_msg = ctk.CTkLabel(welcome_frame,text="Select User",text_color="white",font=ctk.CTkFont("Rounded Arial",size = 30,weight="normal"))
-    welcome_msg.pack_propagate(True)
-    signin_msg.pack()
 
 
 
