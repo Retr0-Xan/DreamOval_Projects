c4d4a148b9f2c762bb26343cb05310ef427e5d35
Retr0-Xan <mayitey69@gmail.com>
2023-10-09 08:55:13 +0000

    test brach

--- Recons Automation - GUI/test.py
+++ Recons Automation - GUI/test.py
@@ -1,19 +1,4 @@
-import customtkinter as ctk
-import tkinter as tk
-from tkinter import ttk
+import os
+path = os.getcwd()
 
-root = ctk.CTk()
-
-root.title("Test")
-root.option_add("*tearOff", False)
-root._set_appearance_mode("light")
-root.geometry("500x550")
-
-yesterday_frame = ttk.LabelFrame(root,text="Set Recons Date",width=500,height=400)
-yesterday_frame.pack()
-yesterday_frame.pack_propagate(False)
-
-yesterady_msg = ctk.CTkLabel(yesterday_frame, text="Recons for yesterday?",text_color="green")
-yesterady_msg.pack()
-
-root.mainloop()
+print(path)
