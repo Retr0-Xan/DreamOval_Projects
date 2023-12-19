969a3476b25b8a24c5a50fe5ac6de144e9f46c27
Retr0-Xan <mayitey69@gmail.com>
2023-12-19 15:24:05 +0000

    Revenue Assurance and Settlement Auto

---
+++ revenueAssurance/WRA.py
@@ -0,0 +1,247 @@
+import pandas as pd
+import customtkinter as ctk
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import csv
+from tkinter import filedialog
+from PIL import Image
+
+
+def choose_file():
+    root.filename = filedialog.askopenfilename(
+        initialdir="/.",
+        title="open file",
+        filetypes=(
+            ("All Files", "*.*"),
+            ("CSV Files", "*.csv"),
+            ("Excel Files", "*.xlsx"),
+        ),
+    )
+    return root.filename
+
+
+def get_data():
+    global all_items
+    file = pd.read_excel(choose_file())
+    merchants = file["merchantName"].drop_duplicates()
+    merchants = merchants.tolist()
+
+    highestComission = 0
+    highestMerchant = ""
+    for merchant in merchants:
+        singleMerchantDf = file.loc[file["merchantName"] == merchant]
+
+        count = len(singleMerchantDf)
+        sum = singleMerchantDf["amount"].sum()
+
+        cost = find_cost(df=singleMerchantDf)
+        commission = find_commission(df=singleMerchantDf)
+
+        if commission > highestComission:
+            highestComission = commission
+            highestMerchant = merchant
+
+        merchant_data = (merchant, count, sum, cost, commission)
+        table.insert(parent="", index=tk.END, values=merchant_data)
+
+    all_items = [table.item(item) for item in table.get_children()]
+
+
+def export_data(tree, filename):
+    # Get column names
+    columns = tree["columns"]
+
+    # Open a CSV file for writing
+    with open(filename, "w", newline="") as csvfile:
+        # Create a CSV writer object
+        csv_writer = csv.writer(csvfile)
+
+        # Write header row with column names
+        csv_writer.writerow(columns)
+
+        # Write data rows
+        for item in tree.get_children():
+            # Get data for each row
+            values = tuple(map(str, tree.item(item, "values")))
+
+            # Write the row to the CSV file
+            csv_writer.writerow(values)
+
+
+def filter_data():
+    filter_text = filter_var.get().lower()
+    table.delete(*table.get_children())
+    filtered_items = [
+        item for item in all_items if filter_text in str(item["values"]).lower()
+    ]
+    for item in filtered_items:
+        table.insert("", "end", values=item["values"])
+
+
+def find_cost(df: pd.DataFrame):
+    cost = 0.00
+    for i in range(0, len(df)):
+        if df.iloc[i]["paymentMode"] == "CASH":
+            cost += 0.00
+        elif df.iloc[i]["paymentMode"] == "VODAFONE_CASH":
+            cost += 1 / 100 * (df.iloc[i]["amount"])
+        elif (
+            df.iloc[i]["paymentMode"] == "MASTERCARD"
+            or df.iloc[i]["paymentMode"] == "VISA"
+        ):
+            cost += 2 / 100 * (df.iloc[i]["amount"])
+        elif df.iloc[i]["paymentMode"] == "MTN_MONEY":
+            if df.iloc[i]["amount"] < 1000:
+                cost += 1 / 100 * (df.iloc[i]["amount"])
+            elif df.iloc[i]["amount"] > 999:
+                cost += 10.00
+    return cost
+
+
+def find_commission(df: pd.DataFrame):
+    commission = 0.00
+    for i in range(0, len(df)):
+        commission += df.iloc[i]["markupFee"] + df.iloc[i]["commissionAmount"]
+
+    return commission
+
+
+root = ctk.CTk(fg_color="#e3e8e4")
+root.title("Revenue Assurance Generator")
+root.option_add("*tearOff", False)
+root._set_appearance_mode("light")
+root.geometry("1260x600")
+style = ttk.Style(root)
+root.tk.call(
+    "source",
+    "/Users/markayitey/Documents/ShadProj/Forest-ttk-theme-master/forest-light.tcl",
+)
+style.theme_use("forest-light")
+
+merchants = []
+
+menu_frame = ctk.CTkFrame(
+    root, width=200, height=root.winfo_height(), fg_color="white"
+)
+menu_frame.pack_propagate(False)
+menu_frame.pack(side="left", fill="y")
+
+
+logo_img = ctk.CTkImage(
+    Image.open("revenueAssurance/assets/KowriLogo.png"), size=(180, 60)
+)
+label = ctk.CTkLabel(menu_frame, image=logo_img, text="")
+label.pack()
+
+dashboard_img = ctk.CTkImage(
+    Image.open("revenueAssurance/assets/monitor.png"), size=(38, 38)
+)
+history_img = ctk.CTkImage(
+    Image.open("revenueAssurance/assets/history.png"), size=(38, 38)
+)
+export_img = ctk.CTkImage(
+    Image.open("revenueAssurance/assets/export.png"), size=(15, 15)
+)
+plus_img = ctk.CTkImage(Image.open("revenueAssurance/assets/plus.png"), size=(15, 15))
+
+
+dashboard_btn = ctk.CTkButton(
+    menu_frame,
+    text="Dashboard",
+    text_color="black",
+    hover_color="#7bed96",
+    fg_color="#48db6b",
+    height=70,
+    corner_radius=0,
+    font=ctk.CTkFont("Helvetica", size=20),
+    image=dashboard_img,
+    compound="left",
+)
+dashboard_btn.pack(pady=18, fill="x")
+generate_btn = ctk.CTkButton(
+    menu_frame,
+    text="Generate",
+    text_color="black",
+    hover_color="#7bed96",
+    fg_color="#48db6b",
+    height=70,
+    corner_radius=0,
+    font=ctk.CTkFont("Helvetica", size=20),
+)
+generate_btn.pack(pady=18, fill="x")
+history_btn = ctk.CTkButton(
+    menu_frame,
+    text="History",
+    text_color="black",
+    hover_color="#7bed96",
+    fg_color="#48db6b",
+    height=70,
+    corner_radius=0,
+    font=ctk.CTkFont("Helvetica", size=20),
+    image=history_img,
+    compound="left",
+)
+history_btn.pack(pady=18, fill="x")
+
+option_frame = ctk.CTkFrame(root, fg_color="#e3e8e4", height=100, corner_radius=0)
+option_frame.pack(fill="x", pady=15)
+filter_var = tk.StringVar()
+search_bar = ctk.CTkEntry(
+    option_frame,
+    width=350,
+    height=10,
+    fg_color="#d4d4d4",
+    corner_radius=0,
+    text_color="green",
+    textvariable=filter_var,
+    border_width=0,
+)
+search_bar.grid(column=0, row=0,padx=(250,0))
+
+search_btn = ctk.CTkButton(
+    option_frame, text="Search", fg_color="green", width=15, command=filter_data
+)
+search_btn.grid(column=1, row=0, padx=(6,30))
+
+select_btn = ctk.CTkButton(
+    option_frame,
+    text="Select file",
+    command=get_data,
+    fg_color="#046924",
+    text_color="white",
+    bg_color="#046924",
+    hover_color="#18ad48",
+    image=plus_img,
+    compound="left",
+)
+select_btn.grid(column=5, row=0, padx=(0,30))
+
+export_btn = ctk.CTkButton(
+    option_frame,
+    text="Export Data",
+    fg_color="#046924",
+    text_color="white",
+    bg_color="#046924",
+    hover_color="#18ad48",
+    command=lambda: export_data(table, "RAG_(month).csv"),
+    image=export_img,
+    compound="left",
+)
+export_btn.grid(column=6, row=0)
+
+table_frame = ctk.CTkFrame(root)
+table_frame.pack()
+table = ttk.Treeview(
+    table_frame,
+    columns=("Merchants", "Total Volume", "Total Value", "Total Cost", "Total Comission"),
+    show="headings",
+    height=20,
+)
+for col in table["columns"]:
+    table.heading(col, text=col, anchor="center")
+    table.column(col, anchor="center")
+table.grid(column=0, row=5, columnspan=3)
+
+
+root.mainloop()
---
+++ settlementAuto/settlementAuto.py
@@ -0,0 +1,135 @@
+import pandas as pd
+from pandas import *
+from openpyxl import load_workbook
+from datetime import date
+
+bulk_gen_df = pd.read_excel(
+    "settlementAuto/kowri_payment_file_generator.xlsm", skiprows=6
+)
+
+settlement_advice_file = "settlement_advice.csv"
+settle_adv_df = pd.read_csv(settlement_advice_file)
+
+
+def clear_rows_with_value(sheet, column):
+    for row in range(sheet.max_row, 1, -1):
+        cell_value = sheet.cell(row=row, column=column).value
+        if (cell_value== f"=_xlfn.VALUETOTEXT(IF(ISNA(VLOOKUP(A{row},'Institutional Codes'!$A$1:$B$24,2,FALSE)),\"\",VLOOKUP(A{row},'Institutional Codes'!$A$1:$B$24,2,FALSE)))"):
+            sheet.delete_rows(row)
+
+
+wb = load_workbook("settlementAuto/kowri_payment_file_generator.xlsm")
+sheet = wb[wb.sheetnames[0]]
+first_col = wb.active.min_column
+last_col = wb.active.max_column
+first_row = wb.active.min_row
+last_row = wb.active.max_row
+
+codes = {
+    "300303": "Absa Bank",
+    "300302": "Standard Chartered Bank",
+    "300304": "GCB Bank",
+    "300305": "National Investment Bank",
+    "300307": "Agricultural Development Bank",
+    "300309": "Universal Merchant Bank",
+    "300310": "Republic Bank",
+    "300311": "Zenith Bank Limited",
+    "300312": "Ecobank Ghana",
+    "300313": "CAL Bank",
+    "300317": "Prudential Bank Limited",
+    "300318": "Stanbic Bank",
+    "300322": "Guaranty Trust (GH) Limited",
+    "300325": "United Bank of Africa",
+    "300329": "Access Bank Limited",
+    "300334": "First National Bank",
+    "300487": "Kowri",
+    "23354": "MTN Mobile Money",
+    "23324": "MTN Mobile Money",
+    "23355": "MTN Mobile Money",
+    "23359": "MTN Mobile Money",
+    "23325": "MTN Mobile Money",
+    "23356": "AirtelTigo Money",
+    "23357": "AirtelTigo Money",
+    "23326": "AirtelTigo Money",
+    "23327": "AirtelTigo Money",
+    "23320": "Vodafone Cash",
+    "23350": "Vodafone Cash",
+    "300323": "Fidelity Bank",
+    "300320": "Bank of Africa",
+    "300316": "First Atlantic Bank",
+    "300319": "FBN Bank",
+}
+institution_codes = {
+    "Kowri": "kowri",
+    "MTN Mobile Money": "mtn-money",
+    "AirtelTigo Money": "airteltigo-money",
+    "Vodafone Cash": "vodafone-cash",
+    "National Investment Bank": "nib-account-fi-service",
+    "Prudential Bank Limited": "prudential-account-fi-service",
+    "Guaranty Trust (GH) Limited": "gt-account-fi-service",
+    "First National Bank": "First National Bank fnb-account-fi-service",
+    "Universal Merchant Bank": "umb-account-fi-service",
+    "Zenith Bank Limited": "zenith-account-fi-service",
+    "Access Bank Limited": "access-account-fi-service",
+    "CAL Bank": "cal-account-fi-service",
+    "Standard Chartered Bank": "standardchartered-account-fi-service",
+    "Ecobank Ghana": "ecobank-account-fi-service",
+    "Absa Bank": "absa-account-fi-service",
+    "GCB Bank": "gcb-account-fi-service",
+    "Stanbic Bank": "stanbic-account-fi-service",
+    "Agricultural Development Bank": "adb-account-fi-service",
+    "United Bank of Africa": "uba-account-fi-service",
+    "FBN Bank": "fnb-account-fi-service",
+    "Fidelity Bank": "fidelity-account-fi-service",
+    "First Atlantic Bank": "first-atlantic-account-fi-service",
+    "Republic Bank": "republic-account-fi-service",
+    "Bank of Africa": "bank-africa-account-fi-service",
+}
+
+unfinished_settlements = []
+sett_accNum = settle_adv_df["Account Number"].astype(str)
+rowNum = 0
+useRow = 7
+for count, (i, acc) in enumerate(enumerate(sett_accNum)):
+    rowNum = i + 7
+
+    if acc.startswith("233"):
+        useAcc = acc[0:5]
+        skiprowBool = False
+        useRow += 1
+
+    elif acc.startswith("300"):
+        useAcc = acc[0:6]
+        skiprowBool = False
+        useRow += 1
+    else:
+        if acc == "nan":
+            unfinished_settlements.append(settle_adv_df["Service Name"].iloc[i])
+        else:
+            unfinished_settlements.append(acc)
+        if skiprowBool:
+            continue
+        else:
+            skiprowBool = True
+        continue
+    if useAcc in codes:
+        print(f"the account {acc} is {codes[useAcc]}")
+        sheet[f"B{useRow}"].value = acc
+        sheet[f"A{useRow}"].value = codes[useAcc]
+        sheet[f"C{useRow}"].value = settle_adv_df["Account Name"].iloc[i]
+        sheet[f"D{useRow}"].value = settle_adv_df["Settlement Amount"].iloc[i]
+        sheet[f"E{useRow}"].value = f"{acc[0:11]}_{date.today()}"
+        sheet[
+            f"F{useRow}"
+        ].value = f"KB_Settlement_{settle_adv_df['Service Name'].iloc[i]}"
+        sheet[f"G{useRow}"].value = institution_codes[codes[useAcc]]
+    else:
+        useRow -= 1
+        continue
+column_to_check = 7
+
+pd.DataFrame([unfinished_settlements]).to_excel("Manual_settlements_pending.xlsx",index=False)
+clear_rows_with_value(sheet, column_to_check)
+wb.close()
+wb.save(f"bulk_settlement_{date.today()}.xlsm")
+print(unfinished_settlements)
